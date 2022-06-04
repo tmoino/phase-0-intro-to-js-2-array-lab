cc32a9137ea818073402b36fc350e6ce3a47ad7e
tmoino <tony.moino@student.moringaschool.com>
2022-06-04 16:55:15 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+    return cats;
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+function appendCat(){
+}
+function appendCat(){
+    var newArray = cats.slice();
+    newArray.push("Broom")
+    return newArray;
+} 
+function prependCat(){
+    var newArray= cats.slice();
+    newArray.unshift("Arnold");
+    return newArray;
+}
+function removeLastCat(){
+    var newArray=cats.slice();
+    newArray.pop();
+    return newArray;
+}
+
+function removeFirstCat(){
+    var newArray= cats.slice();
+    newArray.shift();
+    return newArray;
+}
+
